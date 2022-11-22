c6b90311602ccb0f31a80a21108bf90536f595df
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2022-11-22 21:11:41 +0600

    % feature added

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const display= document.getElementById('display');
 document.getElementById('calc-btns').addEventListener('click',function(event){
-    console.log(event.target.innerText);
     const clickedBtn =event.target.innerText;
     if(clickedBtn=='C'){
         display.value='';
@@ -9,14 +8,26 @@ document.getElementById('calc-btns').addEventListener('click',function(event){
         const result = eval(display.value);
         display.value = Math.sqrt(result);
     }
-    else if(clickedBtn=='<'){
-        display.value=display.value;
+    else if(clickedBtn=='<'){        
+        let result = display.value;
+        result = result.substring(0, result.length - 1);
+        display.value = result;
     }
     else if(clickedBtn=='='){
-        const result = eval(display.value);
+        let result="";
+        if (display.value.includes("%")) {
+            const numbers = display.value.split("%");
+            const number1 = parseInt(numbers[0]);
+            const number2 = parseInt(numbers[1]);
+            result = (number1*100)/number2;
+            result = result.toString();
+        }
+        else{
+            result = eval(display.value);        
+        }
         display.value = result;
     }
     else{
         display.value = display.value+clickedBtn;
     }
-})
+});
